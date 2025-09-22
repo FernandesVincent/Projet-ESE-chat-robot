3f625bea6e9c70f0234b4773d1dbf12bc612e00c
Kelly Luo <kkelly.luo@gmail.com>
2025-09-22 16:14:13 +0200

    code tof avec nucleo

---
+++ software/tof_nucleo/Core/Inc/tof.h
@@ -0,0 +1,31 @@
+/*
+ * tof.h
+ *
+ *  Created on: Sep 22, 2025
+ *      Author: Kelly
+ */
+
+#ifndef INC_TOF_H_
+#define INC_TOF_H_
+
+#include "gpio.h"
+
+typedef struct {
+	GPIO_TypeDef *port;  // GPIO port (GPIOA, GPIOB, etc.)
+	uint16_t pin;        // GPIO pin (GPIO_PIN_0, GPIO_PIN_1, ...)
+} Gpio;
+
+typedef struct {
+	uint16_t dev_address;	// slave address of TOF device
+	Gpio shutdown_pin;		// shutdown pin of TOF device
+	Gpio int_pin;			// INT pin of TOF device (to trigger interruption when measure is done)
+} tof_dev;
+
+
+void tof_boot();
+void tof_initialization();
+void tof_enable_ranging();
+void tof_callback(uint16_t GPIO_Pin);
+
+
+#endif /* INC_TOF_H_ */
