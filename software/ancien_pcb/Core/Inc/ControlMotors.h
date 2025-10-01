d2b9fae9616462852dce207e6dfe02e2612d942f
CaillaudPaul <pcaillaud0709@gmail.com>
2025-10-01 18:49:32 +0200

    started code for motor

---
+++ software/ancien_pcb/Core/Inc/ControlMotors.h
@@ -0,0 +1,25 @@
+/*
+ * ControlMotors.h
+ *
+ *  Created on: Oct 1, 2025
+ *      Author: pcail
+ */
+
+#ifndef INC_CONTROLMOTORS_H_
+#define INC_CONTROLMOTORS_H_
+#include "tim.h"
+
+#define M_FORWARD 'F'
+#define M_REVERSE 'R'
+#define M_STOP 'S'
+
+//Période d'échantillonage à rajouter? Celle du lidar?
+#define L xx //width between the two wheels
+
+
+
+
+
+
+
+#endif /* INC_CONTROLMOTORS_H_ */
