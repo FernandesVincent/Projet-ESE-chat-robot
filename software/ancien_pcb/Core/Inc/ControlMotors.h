995caf77a71ec16a0d878dada3dda770a6efe9e9
CaillaudPaul <pcaillaud0709@gmail.com>
2025-10-10 11:11:48 +0200

    ControlMotor.h

--- software/ancien_pcb/Core/Inc/ControlMotors.h
+++ software/ancien_pcb/Core/Inc/ControlMotors.h
@@ -16,10 +16,26 @@
 //Période d'échantillonage à rajouter? Celle du lidar?
 #define L xx //width between the two wheels
 
+// Structure for the motor
+typedef struct Motor_HandleTypeDef {
+	TIM_HandleTypeDef* pwm_timer;    // Timer for the PWM
+	uint32_t channel;                // Channel for the timer
+	char direction;                  // Mode/direction of the motor
+	int spd;                         // speed of the motor
+} Motor_HandleTypeDef;
 
 
+//Structure for the robot
+//a faire après que les moteurs tournent
 
 
+//prototyping the functions
+void Motor_Init(Motor_HandleTypeDef* motor, TIM_HandleTypeDef* timer, uint32_t channel);
+void Motor_SetSpeed(Motor_HandleTypeDef* motor, int spd);
+void Motor_SetDir(Motor_HandleTypeDef* motor, char direction);
+void Motor_Start(Motor_HandleTypeDef* motor);
+void Motor_Stop(Motor_HandleTypeDef* motor);
+//reste les prototypes du robot à faire
 
 
 #endif /* INC_CONTROLMOTORS_H_ */
--- software/ancien_pcb/Core/Src/ControlMotors.c
+++ software/ancien_pcb/Core/Src/ControlMotors.c
@@ -5,4 +5,21 @@
  *      Author: pcail
  */
 
+#include "ControlMotors.h"
+#include <stdlib.h>
 
+#define pi 3.1415
+#define PWM_MAX 8500 	//à vérifier avec le microcontroller qu'on a
+#define SPEED_MAX 500	//idem
+
+void Motor_Init(Motor_HandleTypeDef* motor, TIM_HandleTypeDef* timer, uint32_t channel){
+	motor->pwm_timer = timer; //assigning pointers to initial values
+	motor->channel = channel;
+	motor->direction = M_STOP;
+	motor->spd = 0;
+}
+
+void Motor_SetSpeed(Motor_HandleTypeDef* motor, int speed){
+	int PWMspeed = (int)(speed * PWM_MAX / SPEED_MAX);
+
+}
