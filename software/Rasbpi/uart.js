bbf8ac0695b1d1901d3924f12c7de1ac58e0f3e9
FernandesVincent <vincent.fernandes@ensea.fr>
2025-10-12 17:26:51 +0200

    gitignore for software folders

--- software/Rasbpi/uart.js
+++ software/Rasbpi/uart.js
@@ -13,12 +13,31 @@ const port = new SerialPort(options);
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));
 
-// let dataRaw = '';
 parser.on('data', data => {
   console.log('Reçu depuis la Nucleo:', data);
   console.log(typeof data);
-  // dataRaw = data.toString();
+
+  const dataObj = JSON.parse(data);
+
+  const TOF1 = dataObj.TOF1;
+  const TOF2 = dataObj.TOF2;
+  const TOF3 = dataObj.TOF3;
+  const TOF4 = dataObj.TOF4;
+  const TOF5 = dataObj.TOF5;
+  const TOF6 = dataObj.TOF6;
+  const LIDAR_D = dataObj.LIDAR_D;
+  const LIDAR_THETA = dataObj.LIDAR_THETA;
+  const speed = dataObj.speed;
+  const accel = dataObj.accel;
+  const batt = dataObj.batt;
+  const role = dataObj.role;
+  const cat_as_not = dataObj.cat_as_not;
+  const mouse_as_not = dataObj.mouse_as_not;
+
+  console.log("TOF1 =", TOF1, "Speed =", speed, "Battery =", batt);
 });
+
+
 
 port.on('open', () => {
   console.log('Port série ouvert');
