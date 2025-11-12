d1d9b4734964bc707152adfcfb189f42c97b2024
CaillaudPaul <pcaillaud0709@gmail.com>
2025-11-12 12:03:22 +0100

    continuing on motors

--- software/ancien_pcb/Core/Inc/ControlMotors.h
+++ software/ancien_pcb/Core/Inc/ControlMotors.h
@@ -14,7 +14,7 @@
 #define M_STOP 'S'
 
 //Période d'échantillonage à rajouter? Celle du lidar?
-#define L xx //width between the two wheels
+#define L 0.17 //width between the two wheels in m
 
 // Structure for the motor
 typedef struct Motor_HandleTypeDef {
@@ -25,6 +25,7 @@ typedef struct Motor_HandleTypeDef {
 } Motor_HandleTypeDef;
 
 
+//Structure for the robot
 typedef struct Mvt_frame {
 	char direction;		//direction
 	float speed;		//vitesse
@@ -33,21 +34,23 @@ typedef struct Mvt_frame {
 	uint8_t role;		//role
 	struct Motor_HandleTypeDef Right_Motor;	//2 moteurs
 	struct Motor_HandleTypeDef Left_Motor;
-};
-
-
-//Structure for the robot
-//a faire après que les moteurs tournent
+} Robot_struct;
 
 
 //prototyping the functions
+//motors
 void Motor_Init(Motor_HandleTypeDef* motor, TIM_HandleTypeDef* timer, uint32_t channel);
 void Motor_SetSpeed(Motor_HandleTypeDef* motor, int spd);
 void Motor_SetDir(Motor_HandleTypeDef* motor, char direction);
 void Motor_Forward(Motor_HandleTypeDef* motor);
 void Motor_Reverse(Motor_HandleTypeDef* motor);
 void Motor_Stop(Motor_HandleTypeDef* motor);
-//reste les prototypes du robot à faire
+//robot
+void Robot_Init(Robot_struct* robot, Motor_HandleTypeDef* motor_R, Motor_HandleTypeDef* motor_L);
+void Robot_Forward(Robot_struct* robot);
+void Robot_Reverse(Robot_struct* robot);
+void Robot_Stop(Robot_struct* robot);
+void Robot_SetAngle(Robot_struct* robot, float angle);
 
 
 #endif /* INC_CONTROLMOTORS_H_ */
--- software/ancien_pcb/Core/Src/ControlMotors.c
+++ software/ancien_pcb/Core/Src/ControlMotors.c
@@ -9,9 +9,10 @@
 #include <stdlib.h>
 
 #define pi 3.1415
-#define PWM_MAX 8500 	//à vérifier avec le microcontroller qu'on a
-#define SPEED_MAX 500	//idem
+#define PWM_MAX 8500 	//ARR+1
+#define SPEED_MAX 100	//so speed between -100 et 100 so that when 100 we have PWMspeed=PWM_MAX
 
+//functions motor
 void Motor_Init(Motor_HandleTypeDef* motor, TIM_HandleTypeDef* timer, uint32_t channel){
 	motor->pwm_timer = timer; //assigning pointers to initial values
 	motor->channel = channel;
@@ -20,17 +21,17 @@ void Motor_Init(Motor_HandleTypeDef* motor, TIM_HandleTypeDef* timer, uint32_t c
 }
 
 void Motor_SetSpeed(Motor_HandleTypeDef* motor, int speed){
-	int PWMspeed = (int)(speed * PWM_MAX / SPEED_MAX);
+	int PWMspeed = (int)(abs(speed) * PWM_MAX / SPEED_MAX);
 	motor->spd = speed;
 
 	if(speed >= 0){
 		Motor_SetDir(motor, 'F');
-		__HAL_TIME_SET_COMPARE(motor->pwm_timer, motor->channel, PWMspeed);
+		__HAL_TIM_SET_COMPARE(motor->pwm_timer, motor->channel, PWMspeed);
 	}
 
 	if(speed <= 0){
 		Motor_SetDir(motor, 'R');
-		__HAL_TIME_SET_COMPARE(motor->pwm_timer, motor->channel, abs(PWMspeed));
+		__HAL_TIM_SET_COMPARE(motor->pwm_timer, motor->channel, PWMspeed);
 	}
 }
 
@@ -54,20 +55,51 @@ void Motor_SetDir(Motor_HandleTypeDef* motor, char direction){
 	}
 }
 
-	void Motor_Forward(Motor_HandleTypeDef* motor){
-		Motor_SetDir(motor, 'F');
-	}
+void Motor_Forward(Motor_HandleTypeDef* motor){
+	Motor_SetDir(motor, 'F');
+}
 
-	void Motor_Reverse(Motor_HandleTypeDef* motor){
-		Motor_SetDir(motor, 'R');
-	}
+void Motor_Reverse(Motor_HandleTypeDef* motor){
+	Motor_SetDir(motor, 'R');
+}
 
-	void Motor_Stop(Motor_HandleTypeDef* motor){
-		Motor_SetDir(motor, 'S');
-	}
+void Motor_Stop(Motor_HandleTypeDef* motor){
+	Motor_SetDir(motor, 'S');
+}
 
-	//reste fonctions du robot avec les 2 moteurs
+//functions robots
+void Robot_Init(Robot_struct* robot, Motor_HandleTypeDef* motor_R, Motor_HandleTypeDef* motor_L){
+	robot->Right_Motor = motor_R;
+	robot->Left_Motor = motor_L;
+	robot->direction = 'S';
+	robot->role = 0; //CAN CHANGE, FOR NOW 0 IS FOR CAT AND ONE FOR MOUSE
+	robot->speed = 0;
+	robot->omega = 0;
+	robot->theta = 0;
+}
 
+void Robot_Forward(Robot_struct* robot){
+	Motor_SetSpeed(robot->Right_Motor, 40);		//speed est à definir entre +ou- SPEED_MAX
+	Motor_SetSpeed(robot->Left_Motor, 40);		//il peut aussi y avoir un delta entre les deux car moteurs pas formement 100% identiques
+	robot->direction = 'F';
+}
+
+void Robot_Reverse(Robot_struct* robot){
+	Motor_SetSpeed(robot->Right_Motor, -40);		//speed est à definir entre +ou- SPEED_MAX
+	Motor_SetSpeed(robot->Left_Motor, -40);
+	robot->direction = 'R';
+}
+
+void Robot_Stop(Robot_struct* robot){
+	Motor_Stop(robot->Right_Motor);
+	Motor_Stop(robot->Left_Motor);
+	robot->direction = 'S';
+}
+
+void Robot_Angle(Robot_struct* robot, float angle){		//calcule les nouvelles vitesse pour que le robot tourne vers la cible
+														//puis remet la vietesse pour qu'il aille droit
+	;
+}
 
 
 
