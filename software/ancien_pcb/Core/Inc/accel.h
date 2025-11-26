89084e01caf04dd1c6e58180f125d124f5a7c398
CaillaudPaul <pcaillaud0709@gmail.com>
2025-11-26 12:29:25 +0100

    Working on ADXL

--- software/ancien_pcb/Core/Inc/accel.h
+++ software/ancien_pcb/Core/Inc/accel.h
@@ -11,17 +11,71 @@
 #include <stdio.h>
 #include "i2c.h"
 
-#define ADXL_address (0x53 << 1)		//adxl353 has i2c on 7 bits so need to put it on 8 bits
+#define ADXL_address (0x53 << 1)		//adxl343 has i2c on 7 bits so need to put it on 8 bits
 #define ADXL_DEVID (0xe5)				//The DEVID register holds a fixed device ID code of 0xE5 (345 octal) (datasheet)
 
-#define ADXL_REG_DEVID (0x00) 			//REG is for register def (datasheet p.23)
-#define ADXL_REG_THRESH_TAP (0x1d)
-#define ADXL_REG_OFSX (0x1e)
-#define ADXL_REG_OFSY (0x1f)
-#define ADXL_REG_OFSZ (0x20)
-#define ADXL_REG_DUR (0x21)
-#define ADXL_REG_ACT_INAT_CTL (0x27)
-#define ADXL_REG_
+#define ADXL_REG_DEVID 				(0x00) 			//REG is for register def (datasheet p.23)
+#define ADXL_REG_THRESH_TAP 		(0x1d)
+#define ADXL_REG_OFSX 				(0x1e)
+#define ADXL_REG_OFSY 				(0x1f)
+#define ADXL_REG_OFSZ 				(0x20)
+#define ADXL_REG_DUR 				(0x21)
+#define ADXL_REG_ACT_INAT_CTL 		(0x27)
+#define ADXL_REG_TAP_AXES 			(0x2a)
+#define ADXL_REG_ACT_TAP_STATUS 	(0x2b)
+#define ADXL_REG_BW_RATE 			(0x2c)
+#define ADXL_REG_POWER_CTL 			(0x2d)
+#define ADXL_REG_INT_ENABLE 		(0x2e)
+#define ADXL_REG_INT_MAP 			(0x2f)
+#define ADXL_REG_INT_SOURCE 		(0x30)
+#define ADXL_REG_DATA_FORMAT 		(0x31)
+#define ADXL_REG_DATAX0				(0x32)
+#define ADXL_REG_DATAX1				(0x33)
+#define ADXL_REG_DATAY0				(0x34)
+#define ADXL_REG_DATAY1				(0x35)
+#define ADXL_REG_DATAZ0				(0x36)
+#define ADXL_REG_DATAZ1				(0x37)
+#define ADXL_REG_FIFO_CTL 			(0x38)
 
+#define ADXL_I2C hi2c
+
+typedef struct ADXL{
+	float accelx;
+	float accely;
+	float accelz;
+	uint8_t devID;
+	uint8_t range;
+	uint8_t rate;
+	I2C_HandleTypeDef *hi2c;
+}ADXL343_STRUCT;
+
+typedef enum{
+	RATE_3200HZ = 0x0f,		//output data rate (different current consumption for each rate)
+	RATE_1600HZ = 0x0e,
+	RATE_800HZ = 0x0d,
+	RATE_400HZ = 0x0c,
+	RATE_200HZ = 0x0b,
+	RATE_100HZ = 0x0a,
+	RATE_50HZ = 0x09,
+	RATE_25HZ = 0x08,
+	RATE_12_5HZ = 0x07,
+	RATE_6_25HZ = 0x06,
+	RATE_3_13HZ = 0x05,
+	RATE_1_56HZ = 0x04,
+	RATE_0_78HZ = 0x03,
+	RATE_0_39HZ = 0x02,
+	RATE_0_20HZ = 0x01,
+	RATE_0_1OHZ = 0x00
+}ADXL_Rate;
+
+typedef enum{
+	RANGE_2G = 0b00,		//to set the g range of the ADXL343
+	RANGE_4G = 0b01,
+	RANGE_8G = 0b10,
+	RANGE_16G = 0b11
+}ADXL_Range;
+
+
+void ADXL_Init(ADXL343_STRUCT *accel, I2C_HandleTypeDef *hi2c);
 
 #endif /* INC_ACCEL_H_ */
--- software/ancien_pcb/Core/Src/accel.c
+++ software/ancien_pcb/Core/Src/accel.c
@@ -9,3 +9,25 @@
 #include <stdio.h>
 //#include "cmsis_os.h"
 
+static void ADXL_I2C_Read(){
+
+}
+
+static void ADXL_I2C_Write(){
+
+}
+
+void AXDL_Init(ADXL343_STRUCT *accel, I2C_HandleTypeDef *hi2c){
+	if(accel == NULL){
+		prinf("Accel not found");
+	}
+
+	accel->accelx = 0.;
+	accel->accely = 0.;
+	accel->accelz = 0.;
+	accel->devID = 1;
+	accel->hi2c = hi2c;
+	uint8_t configData;
+
+
+}
