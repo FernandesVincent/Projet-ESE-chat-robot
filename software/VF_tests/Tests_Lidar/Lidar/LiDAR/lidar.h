00c8e4946b1dfe12351202a5833afb0f60808f7f
FernandesVincent <vincent.fernandes@ensea.fr>
2025-12-16 11:45:18 +0100

    test lidar and TOF ok

---
+++ software/VF_tests/Tests_Lidar/Lidar/LiDAR/lidar.h
@@ -0,0 +1,25 @@
+/*
+ * lidar.h
+ *
+ *  Created on: Oct 8, 2025
+ *      Author: fernandesvincent
+ */
+
+#ifndef __LIDAR_H__
+#define __LIDAR_H__
+
+
+#include "main.h"
+
+#define UART_RX_BUFFER_SIZE 256
+
+typedef struct {
+	float angle;
+	float distance;
+} Valid_point;
+
+void lidar_check_frames();
+void lidar_checksum_test();
+void lidar_get_angle_and_distance();
+
+#endif
